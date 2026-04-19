15cac64d1a98877dcf565c5227c8f3270ebe5c9e
knamo81-dot <knamo81@gmail.com>
2026-04-19 14:38:40 +0900

    Delete supabase(2).js

--- supabase(2).js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = "https://mbqpsovlwvedwrtbbauj.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1icXBzb3Zsd3ZlZHdydGJiYXVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTI2NTksImV4cCI6MjA5MTM4ODY1OX0.B3VWnRUn-A9hABLrx5ysFDQeAJvP_rTktzGiuz5LeTY";
-
-window.ReagentApp = window.ReagentApp || {};
-window.ReagentApp.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
